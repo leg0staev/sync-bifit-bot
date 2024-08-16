a9e891b2d2af129e86c855d0a993fc07b0f90308
legostaev <alexey.legostaev@gmail.com>
2024-08-16 14:31:28 +0300

    добавил обновление токена через рефреш токен

--- Clases/BifitApi/BifitSession.py
+++ Clases/BifitApi/BifitSession.py
@@ -1,6 +1,7 @@
-from Request import Request
-from logger import logger
 import time
+
+from Clases.BifitApi.Request import Request
+from logger import logger
 
 
 class BifitSession(Request):
@@ -14,26 +15,56 @@ class BifitSession(Request):
         self.refresh_token = None
         logger.debug('создал класс сессии бифит-касса')
 
-    async def get_token_async(self):
+    async def initialize(self) -> None:
+        """Асинхронная инициализация токена."""
+        await self.get_token_async()
+
+    async def get_token_async(self) -> None:
+        """Получение токена, если он отсутствует или истек."""
         logger.debug('get_token_async started')
         auth_url = 'https://kassa.bifit.com/cashdesk-api/v1/oauth/token'
-        if self.token is None:
+        if self.expiration_time is None:
             logger.debug('старого токена нет, пробую получить')
-            body = {"username": self.username,
-                    "password": self.password,
-                    "client_id": "cashdesk-rest-client",
-                    "client_secret": "cashdesk-rest-client",
-                    "grant_type": "password"}
+            body = {
+                "username": self.username,
+                "password": self.password,
+                "client_id": "cashdesk-rest-client",
+                "client_secret": "cashdesk-rest-client",
+                "grant_type": "password",
+            }
             response = await self.send_post_async(url=auth_url, data=body)
-            self.token = response.get('token')
-            self.refresh_token = response.get('refresh_token')
-            expires_in = response.get('expires_in')
-            try:
-                if expires_in is None:
-                    raise ValueError("expires_in is None")
-                expires_in = float(expires_in)
-            except (TypeError, ValueError) as e:
-                logger.error(f'Ошибка рассчета времени истечения токена - {e}')
-            else:
-                self.expiration_time = time.time() + expires_in
-            ...
+            # logger.debug(f'ответ сервера - {response}')
+            self.bifit_token_response_parse(response)
+
+        elif self.expiration_time < time.time():
+            logger.debug('старый токен истек, пробую получить новый')
+            body = {
+                "refresh_token": self.refresh_token,
+                "client_id": "cashdesk-rest-client",
+                "client_secret": "cashdesk-rest-client",
+                "grant_type": "refresh_token",
+            }
+            response = await self.send_post_async(url=auth_url, data=body)
+            self.bifit_token_response_parse(response)
+
+    def bifit_token_response_parse(self, response: dict) -> None:
+        """Парсинг ответа на запрос токена."""
+        try:
+            self.token = response['access_token']
+            logger.debug('Достал токен из ответа сервера')
+        except KeyError:
+            logger.error('Отсутствует access_token в ответе сервера - %s', response)
+
+        try:
+            self.refresh_token = response['refresh_token']
+            logger.debug('Достал рефреш токен из ответа сервера')
+        except KeyError:
+            logger.error('Отсутствует refresh_token в ответе сервера - %s', response)
+
+        try:
+            expires_in = float(response['expires_in'])
+            logger.debug('Достал время жизни токена из ответа сервера')
+            self.expiration_time = time.time() + expires_in
+        except (KeyError, ValueError):
+            logger.error('Отсутствует или некорректное значение expires_in в ответе сервера - %s', response)
+
